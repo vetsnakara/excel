ed26113212c9b0c0c4f06e93a65ad6f357ab03ed
Konstantin Arakantsev <kostya3312@gmail.com>
2020-05-14 12:33:39 +0300

    refactor: extract resize logic to ResizeManager

---
+++ src/components/Table/ResizeManager.js
@@ -0,0 +1,132 @@
+import { $ } from '@core/dom'
+
+const RESIZE_TYPE_COL = 'col'
+const RESIZE_TYPE_ROW = 'row'
+
+export class ResizeManager {
+  /**
+   * Resizer element
+   */
+  resizer = null
+
+  /**
+   * Initial cursor coordinates when resizing is started
+   */
+  cursorInitCoords = { x0: 0, y0: 0 }
+
+  /**
+   * Delta of cursor coords through resizing process
+   */
+  cursorDeltaCoords = { dx: 0, dy: 0 }
+
+  /**
+   * Index of column to be resized
+   */
+  colToResize = null
+
+  constructor($root) {
+    this.$root = $root
+  }
+
+  initResize($root, { target, pageX, pageY }) {
+    // resizer element
+    this.resizer = $(target)
+
+    // type of resize (col/row)
+    this.resizeType = this.resizer.data.resize
+
+    this.cursorInitCoords = {
+      x0: pageX,
+      y0: pageY
+    }
+
+    if (this.resizeType === RESIZE_TYPE_COL) {
+      this.colToResize = this.resizer.closest('[data-col]')
+    }
+
+    // listen for mouse move
+    $root.on('mousemove', this.mouseMoveHanldler)
+
+    // finish listen of move and do resize
+    $root.on('mouseup', () => {
+      // unsubscribe
+      $root.off('mousemove', this.mouseMoveHanldler)
+
+      // do resize
+      this.resize()
+
+      // reset
+      this.reset()
+    })
+  }
+
+  mouseMoveHanldler = ({ pageX, pageY }) => {
+    this.updateCursorDelta(pageX, pageY)
+    this.moveResizer()
+  }
+
+  resizeCol() {
+    const colIndex = this.colToResize.data.col
+    const startWidth = parseInt(this.colToResize.css('width'))
+    const cellsToResize = this.$root.findAll(`[data-col="${colIndex}"]`)
+
+    const { dx } = this.cursorDeltaCoords
+
+    cellsToResize.forEach((cell) => {
+      cell.css({ width: `${startWidth + dx}px` })
+    })
+  }
+
+  resizeRow() {
+    const rowToResize = this.resizer.closest('[data-row]')
+    const startHeight = parseInt(rowToResize.css('height'))
+
+    const { dy } = this.cursorDeltaCoords
+
+    rowToResize.css({ height: `${startHeight + dy}px` })
+  }
+
+  updateCursorDelta(x, y) {
+    const dx = x - this.cursorInitCoords.x0
+    const dy = y - this.cursorInitCoords.y0
+
+    this.cursorDeltaCoords.dx = dx
+    this.cursorDeltaCoords.dy = dy
+  }
+
+  moveResizer() {
+    const { dx, dy } = this.cursorDeltaCoords
+
+    if (this.resizeType === RESIZE_TYPE_COL) {
+      this.resizer.css({
+        transform: `translateX(${dx}px)`
+      })
+    } else if (this.resizeType === RESIZE_TYPE_ROW) {
+      this.resizer.css({
+        transform: `translateY(${dy}px)`
+      })
+    }
+  }
+
+  resize() {
+    if (this.resizeType === RESIZE_TYPE_COL) {
+      this.resizeCol()
+    } else if (this.resizeType === RESIZE_TYPE_ROW) {
+      this.resizeRow()
+    }
+  }
+
+  reset() {
+    this.cursorDeltaCoords = { dx: 0, dy: 0 }
+
+    if (this.resizeType === RESIZE_TYPE_COL) {
+      this.resizer.css({
+        transform: `translateX(0)`
+      })
+    } else if (this.resizeType === RESIZE_TYPE_ROW) {
+      this.resizer.css({
+        transform: `translateY(0)`
+      })
+    }
+  }
+}
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -2,109 +2,18 @@ import { Component } from '@/core/Component'
 import { $ } from '@core/dom'
 
 import { getTable } from './template'
-
-const RESIZE_TYPE_COL = 'col'
-const RESIZE_TYPE_ROW = 'row'
+import { ResizeManager } from './ResizeManager'
 
 export class Table extends Component {
   static className = 'table'
 
-  resizing = false
+  resizeManager = new ResizeManager(this.$root)
 
-  screenX = null
-  screenY = null
+  onMousedown({ target }) {
+    const $target = $(target)
 
-  startWidth = null
-
-  colIndex = null
-  cellsToResize = null
-  rowToResize = null
-  delta = null
-
-  onMousedown({ target, screenX, screenY }) {
-    this.resizer = $(target)
-    this.resizeType = this.resizer.data.resize
-
-    if (this.resizeType) {
-      this.resizing = true
-
-      this.screenX = screenX
-      this.screenY = screenY
-
-      if (this.resizeType === RESIZE_TYPE_COL) {
-        const column = this.resizer.closest('[data-col]')
-        const colIndex = column.data.col
-
-        this.cellsToResize = this.$root.findAll(`[data-col="${colIndex}"]`)
-        this.startWidth = parseInt(column.css('width'))
-      } else if (this.resizeType === RESIZE_TYPE_ROW) {
-        this.rowToResize = this.resizer.closest('[data-row]')
-        this.startHeight = parseInt(this.rowToResize.css('height'))
-      }
-    }
-  }
-
-  onMouseup() {
-    if (this.resizing) {
-      this.resize()
-      this.resetResizer()
-      this.resizing = false
-    }
-  }
-
-  onMousemove({ screenX, screenY }) {
-    if (this.resizing) {
-      this.updateCursorDelta(screenX, screenY)
-      this.moveResizer()
-    }
-  }
-
-  resizeColCells() {
-    this.cellsToResize.forEach((cell) => {
-      cell.css({ width: `${this.startWidth + this.delta.x}px` })
-    })
-  }
-
-  resizeRow() {
-    this.rowToResize.css({ height: `${this.startHeight + this.delta.y}px` })
-  }
-
-  updateCursorDelta(x, y) {
-    this.delta = {
-      x: x - this.screenX,
-      y: y - this.screenY
-    }
-  }
-
-  moveResizer() {
-    if (this.resizeType === RESIZE_TYPE_COL) {
-      this.resizer.css({
-        transform: `translateX(${this.delta.x}px)`
-      })
-    } else if (this.resizeType === RESIZE_TYPE_ROW) {
-      this.resizer.css({
-        transform: `translateY(${this.delta.y}px)`
-      })
-    }
-  }
-
-  resize() {
-    if (this.resizeType === RESIZE_TYPE_COL) {
-      this.resizeColCells()
-    } else if (this.resizeType === RESIZE_TYPE_ROW) {
-      this.resizeRow()
-    }
-  }
-
-  resetResizer() {
-    if (this.resizeType === RESIZE_TYPE_COL) {
-      this.resizer.css({
-        transform: `translateX(0)`
-      })
-    } else if (this.resizeType === RESIZE_TYPE_ROW) {
-      this.resizer.css({
-        transform: `translateY(0)`
-      })
+    if ($target.data.resize) {
+      this.resizeManager.initResize(this.$root, event)
     }
   }
 
